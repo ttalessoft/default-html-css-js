4291a27ca181534ac276aa109c3ffa84724e9b07
ttales gold roger <ttalessoft@gmail.com>
2021-03-16 20:11:27 -0300

    refatorando funções do uglify-css

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
 
 
 function compilaSass() {
@@ -23,6 +24,8 @@ function compilaSass() {
 }
 gulp.task('sass', compilaSass);
 
+
+// agrupa todos os js, concatena, converte, comprime e exporta
 function gulpJs() {
   return gulp
     .src('js/main/*.js')
@@ -37,10 +40,34 @@ function gulpJs() {
 
 gulp.task('mainjs', gulpJs);
 
+// css plugins
+function pluginCss() {
+  return gulp
+    .src([
+      'node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
+      'node_modules/bootstrap/dist/css/bootstrap-reboot.min.css',
+      'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+      'node_modules/font-awesome/css/font-awesome.min.css'
+    ])
+    .pipe(concat('plugins.css'))
+    .pipe(uglifycss({
+      'uglyComments': true
+    }))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('plugincss', pluginCss);
+
+
 // js plugins
 function pluginJs() {
   return gulp
     .src([
+      'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
       'node_modules/jquery/dist/jquery.min.js',
       'node_modules/moment/min/moment-with-locales.min.js'
     ])
@@ -63,11 +90,11 @@ function browser() {
 gulp.task('browser-sync', browser);
 
 function watch() {
-  gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch('css/scss/**/*.scss', compilaSass);
   gulp.watch(['js/main/*.js', '!js/main.js'], gulpJs);
   gulp.watch(['*.html', '*.php']).on('change', browserSync.reload);
 }
 
 gulp.task('watch', watch);
 
-gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs', 'pluginjs'));
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs', 'pluginjs', 'plugincss'));
